ea456394596513fd0494bb7a646141d79aa40510
nullaman <45248750@qq.com>
2022-10-14 11:46:54 +0800

    ce

--- pyside6_图形/测试/demo.py
+++ pyside6_图形/测试/demo.py
@@ -13,7 +13,7 @@ from Signal import my_signal
 
 
 class MainWindow(QMainWindow):
-    db_connect = ' '
+    db_connect = ' cesces'
 
     def __init__(self):
         super(MainWindow, self).__init__()
